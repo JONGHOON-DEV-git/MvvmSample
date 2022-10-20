0c232ba48f576a9c44f6846121f738dba89fac72
leejonghoon <jonghoon.1738@gmail.com>
2022-10-20 19:09:43 +0900

    FormTest

---
+++ Mvvm_Example/Mvvm_Example/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvvm_Example
+{
+    public class Model
+    {
+        String ModelName { get; set; }
+        int ModelNO { get; set; }
+    }
+}
